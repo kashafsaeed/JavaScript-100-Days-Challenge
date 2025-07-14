0740c058122911cd05742da983fae425a9f23509
Your Name <you@example.com>
2025-07-15 04:00:41 +0500

    Add Day2 JavaScript practice code

---
+++ Day2/script.js
@@ -0,0 +1,59 @@
+
+
+// Q :
+// var num1 = 8;
+// var num2 = 3;
+
+
+
+//Addition
+var num1 = 8;
+var num2 = 3;
+console.log (num1+num2);  //11
+
+
+//subtraction
+
+var num1 = 8;
+var num2 = 3;
+console.log (num1-num2); //5
+
+
+
+//multiplication
+
+var num1 = 8;
+var num2 = 3;
+console.log (num1*num2); //24
+
+//division
+
+var num1 = 8;
+var num2 = 3;
+console.log (num1/num2);  //2.6
+
+//Modulus
+
+
+var num1 = 8;
+var num2 = 3;
+console.log (num1%num2); //2
+
+
+
+
+//Q:  Ek aur variable banao var count = 5;
+//Us pe ++
+
+var count = 5;
+count++
+console.log (count); //6
+
+
+
+
+
+
+
+
+
