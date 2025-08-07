9bcd38d31a37b37da9b6e1133ab656399ee9f4bd
kashafsaeed7842@gmail.com <kashafsaeed7842@gmail.com.noreply.github.com>
2025-08-07 15:30:42 +0500

    day 3

---
+++ day3/app.js
@@ -0,0 +1,93 @@
+// operators
+// 1: 
+var a = 5 ;
+var b = 90 ;
+var totalvalue = a + b ;
+document.write(" total value is " + totalvalue + "<BR>" );
+
+
+
+// 2:
+var a = 87;
+var b = 45;
+var c = 78;
+var d = 34 ;
+var total = a + b + b - c + d + a;
+document.write(" value is " + total  +  " <br>"); //  95
+
+
+
+// 3 :
+
+var a = 57;
+var b = 48;
+var c = 18;
+var d = 34 ;
+var total = a * b + b * c + d % a;
+document.write(" value is " + total  +  " <br>"); // 3634
+
+
+// 4 :
+
+var a = 23;
+var y = 87;
+var total1 = (23 / 87 ) * 100;
+document.write( " total is " + total1 + " <br>") // 26.436781609195403
+
+
+
+// String Operators
+// 1 
+
+const text1 = 'a';
+const text2 = ' b ';
+const result = text1 > text2;
+document.write ( "result " + result + " <br>") // true
+
+// 2
+
+const text0 = 'a';
+const text = ' b ';
+const result2 = text > text0;
+document.write ( "result " + result2 + " <br>") // false
+
+
+// 3
+const text01 = 'a';
+const text10 = ' b ';
+const result3 = text10 > text01;
+document.write ( "result " + result3 + " <br>") // false
+
+// 4 
+
+const text21 = 'a';
+const tex2t0 = 'b';
+const result4 = text21 < tex2t0;
+document.write ( "result " + result4 + " <br>") // true
+
+
+
+// 5 
+
+const ms1 = 'a';
+const ms2 = 'b';
+const result5 = ms1 < ms2;
+document.write ( "result " + result5 + " <br>"); // ture
+
+
+
+// 6
+
+const ms0 =  'a';
+const ms20 = 'b';
+const result6 = ms0 < ms20;
+document.write ( "result " + result6 + " <br>"); // true
+
+
+// 7 
+ const msg = 'a';
+const msg1 = 'b';
+const result7 = msg1 > msg;
+document.write ( "result " + result7 + " <br>") ; // true
+
+
