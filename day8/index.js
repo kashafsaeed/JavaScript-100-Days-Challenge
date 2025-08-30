f21a08becb7cdf87e54d7c66c41f59e922457007
kashafsaeed7842@gmail.com <kashafsaeed7842@gmail.com.noreply.github.com>
2025-08-30 16:32:38 +0500

    commit

---
+++ day8/index.js
@@ -0,0 +1,10 @@
+var num = 12345;
+console.log(num.toString().split("").reverse().join("")); 
+// Output: 54321
+// Explanation: Similar to the previous example, but starts with a number. It converts the number to a string first before performing the split, reverse, and join operations.
+
+
+var str = "Hello, World!";
+console.log(str.split("").reverse()); // Output: [ '!', 'd', 'l', 'r', 'o', 'W', ' ', ',', 'o', 'l', 'l', 'e', 'H' ]
+// Explanation: The code takes a string, splits it into an array of characters, and then reverses the array.
+
