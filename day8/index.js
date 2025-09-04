3e7d3e311fa4019ec613616fdd2452754068f29d
kashafsaeed7842@gmail.com <kashafsaeed7842@gmail.com.noreply.github.com>
2025-09-04 12:56:06 +0500

    commit

--- day8/index.js
+++ day8/index.js
@@ -8,3 +8,5 @@ var str = "Hello, World!";
 console.log(str.split("").reverse()); // Output: [ '!', 'd', 'l', 'r', 'o', 'W', ' ', ',', 'o', 'l', 'l', 'e', 'H' ]
 // Explanation: The code takes a string, splits it into an array of characters, and then reverses the array.
 
+console.log(num.toString(16)); // Output: 3039
+// Explanation: The code converts the number 12345 to its hexadecimal string representation. The toString(16) method is used to convert the number to base 16 (hexadecimal).
---
+++ day9/app.js
@@ -0,0 +1,20 @@
+  function greet(name) {
+       console.log("Hello, " + name + "!");
+   }
+    greet("Smit"); // Hello, Smit!
+
+
+    greet("John"); // Hello, John!
+    greet("Alice"); // Hello, Alice!
+    greet("Bob"); // Hello, Bob!
+    greet("Eve"); // Hello, Eve!
+
+
+    function add(a, b) {
+        return a + b;
+    }
+    console.log(add(5, 3)); // 8
+    console.log(add(10, 20)); // 30
+    console.log(add(-5, 5)); // 0
+    console.log(add(0, 0)); // 0
+    console.log(add(2.5, 3.5)); // 6
